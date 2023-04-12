bbefe7059cfd6a89bf8b637a19b942f67e34a66e
scpark98 <scpark98@gmail.com>
2023-04-12 15:34:27 +0900

    초기 커밋.

---
+++ Test_UtilLogDlg.h
@@ -0,0 +1,34 @@
+﻿
+// Test_UtilLogDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CTestUtilLogDlg 대화 상자
+class CTestUtilLogDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CTestUtilLogDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_TEST_UTILLOG_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
